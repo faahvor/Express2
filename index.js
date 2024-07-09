dee89eafeeeeb2abdf812572e357518b68b31e74
Favour Ifurukpe <tinafavour2018@gmail.com>
2024-07-09 11:22:44 +0100

    class4

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-import express from "express"
+import express from "express";
+import data from "./data/data.json" assert { type: "json" };
+
+const app = express();
+const PORT = 7777;
+
+//middleware
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+  
+});
+app.post("/fruits",(req,res)=>{
+    console.log(req.body);
+    const body = req.body
+    data.push(body)
+    console.log(data);
+    res.send("successfully pushed")
+    
+})
+
+app.listen(PORT,()=>{
+    console.log(`server is running on http://localhost:${PORT}`);
+})
